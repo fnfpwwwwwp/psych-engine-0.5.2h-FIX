ffdd57b3ee438de63b7a35aad9358f030c1cf3fd
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-01-08 16:39:05 +0800

    fix

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -1,52 +1,25 @@
-import flixel.text.FlxText;
-import flixel.FlxState;
-import flixel.FlxG;
-import flixel.FlxSubState;
-import flixel.FlxBasic;
+Onpackage;
 
-using StringTools;
+import flixel.FlxBasic;
+import flixel.FlxG;
 
 class FlxVideo extends FlxBasic
 {
-	public static var androidPath:String = 'file:///android_asset/';
+	public var finishCallback:Void->Void = null;
 
-	public static var source1:String = 'assets/videos/';
-
-	// public var nextState:FlxState;
-
-    public var finishCallback:Void->Void = null;
-
-	public function new(source:String)
+	public function new(path:String)
 	{
 		super();
 
-		 text = new FlxText(0, 0, 0, "Video Exited! Tap to Continue", 48);
-		 text.screenCenter();
-		 text.alpha = 0;
-		 add(text);
-		 nextState = toTrans;
-		 FlxG.autoPause = false;
-
-	}
-
-	public override function update(dt:Float) {
-		for (touch in FlxG.touches.list)
-			if (touch.justReleased)
-				if(finishCallback != null) finishCallback();
-
-                 if(FlxG.android.justReleased.BACK)
-                {
-                    if(finishCallback != null) finishCallback();
-                 }
-
-		super.update(dt);	
-	}
-
-	public function onClose(){// not working
-	 	trace('video closed lmao');
-		if (finishCallback != null)
+		var video:VideoHandler = new VideoHandler();
+		video.finishCallback = function()
 		{
-			finishCallback();
+			if (finishCallback != null)
+				finishCallback();
 		}
-	 }
+		video.playVideo(SUtil.getStorageDirectory() + path, false, false);
+		else
+		if (finishCallback != null)
+			finishCallback();
+	}
 }
